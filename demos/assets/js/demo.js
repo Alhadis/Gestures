32385283913e81550fc6468297770fd57ab42b00
Alhadis <gardnerjohng@gmail.com>
2016-01-24 22:00:29 +1100

    Use spaces to align columns of code

--- demos/assets/js/demo.js
+++ demos/assets/js/demo.js
@@ -1,5 +1,5 @@
 function cssPrefix(n){
-	var s	=	document.documentElement.style;
+	var s = document.documentElement.style;
 	if(n.toLowerCase() in s) return "";
 	for(var p = "Webkit Moz Ms O Khtml", p = (p.toLowerCase() + p).split(" "), i = 0; i < 10; ++i)
 		if(p[i]+n in s) return p[i];
@@ -12,7 +12,7 @@ function cssPrefix(n){
 (function(o){
 	if(o.END) return;
 
-	var names	=	"transitionend webkitTransitionEnd oTransitionEnd otransitionend".split(" ");
+	var names = "transitionend webkitTransitionEnd oTransitionEnd otransitionend".split(" ");
 	for(var i = 0; i < 4; ++i)
 		if("on"+names[i].toLowerCase() in window)
 			return (o.END = names[i]);
@@ -22,20 +22,20 @@ function cssPrefix(n){
 
 
 /** Okay, this is just getting ridiculous now, John... */
-var Smudge	=	function(x, y){
+var Smudge = function(x, y){
 
-	var node			=	document.createElement("div");
-	node.className		=	"smudge";
-	node.style.left		=	x+"px";
-	node.style.top		=	y+"px";
+	var node        = document.createElement("div");
+	node.className  = "smudge";
+	node.style.left = x+"px";
+	node.style.top  = y+"px";
 
-	var fadedOut	=	function(e){
+	var fadedOut = function(e){
 		node.removeEventListener(TransitionEvent.END, fadedOut);
 		node.parentNode.removeChild(node);
 		clearTimeout(timeout);
 	},
 
-	timeout	=	setTimeout(fadedOut, 3000);
+	timeout = setTimeout(fadedOut, 3000);
 
 
 	node.addEventListener(TransitionEvent.END, fadedOut);
@@ -51,60 +51,60 @@ var Smudge	=	function(x, y){
 
 
 /** Shortcuts */
-var each		=	Array.prototype.forEach;
+var each = Array.prototype.forEach;
 
 
-var container	=	document.getElementById("container");
+var container = document.getElementById("container");
 
 
 /** Define some getter/setter properties on our slides container. */
 (function(elem){
 
-	var	activeClass		=	"active",
-		transform		=	cssPrefix("Transform") || "transform",
+	var activeClass = "active",
+	transform       = cssPrefix("Transform") || "transform",
 
-		/** DOM references */
-		slideList		=	elem.firstElementChild,
-		slides			=	slideList.children,
-		currentSlide	=	null,
+	/** DOM references */
+	slideList       = elem.firstElementChild,
+	slides          = slideList.children,
+	currentSlide    = null,
 
-		/** Private variables */
-		pull			=	0,
-		slide			=	null,
-		dragging		=	false;
+	/** Private variables */
+	pull            = 0,
+	slide           = null,
+	dragging        = false;
 
 
 	/** Set the initial slide index based on the position of first child node with a class of "active". */
 	for(var c, l = slides.length, i = 0; i < l; ++i)
 		if((c = slides[i]).classList.contains(activeClass)){
-			slide			=	i;
-			currentSlide	=	c;
+			slide = i;
+			currentSlide = c;
 			break;
 		}
 
 
 	/** Couldn't find an initial slide to set as active, so we'll just pick the first. */
 	if(slide === null)
-		currentSlide	=	slides[0],
-		slide			=	0;
+		currentSlide  = slides[0],
+		slide         = 0;
 
 
 	/** Get/set the index of the currently active slide. */
 	Object.defineProperty(elem, "slide", {
-		get:	function(){ return slide; },
-		set:	function(i){
-			var numSlides	=	slides.length - 1;
+		get: function(){ return slide; },
+		set: function(i){
+			var numSlides = slides.length - 1;
 
 			/** Sanitise our input: make sure it falls between 0 and slides.length */
-			if(i > numSlides) 	i = dragging ?	numSlides : 0;
-			else if(i < 0)		i = dragging ?	0 : numSlides;
+			if(i > numSlides)  i = dragging ? numSlides : 0;
+			else if(i < 0)     i = dragging ? 0 : numSlides;
 
-			pull	=	0;
-			slideList.style.transform	=	null;
+			pull = 0;
+			slideList.style.transform = null;
 			each.call(slides, function(o){ o.classList.remove(activeClass); });
 
-			slide			=	i;
-			currentSlide	=	slides[i];
+			slide        = i;
+			currentSlide = slides[i];
 			currentSlide.classList.add(activeClass);
 		}
 	});
@@ -115,7 +115,7 @@ var container	=	document.getElementById("container");
 	Object.defineProperty(elem, "pull", {
 		get: function(){ return pull; },
 		set: function(i){
-			pull	=	i;
+			pull = i;
 			slideList.style[transform] = "translateX("+i+"px)";
 		}
 	});
@@ -123,8 +123,8 @@ var container	=	document.getElementById("container");
 
 	/** Basic helper property for toggling the container node's dragClass */
 	Object.defineProperty(elem, "dragging", {
-		get:	function(){ 	return dragging; },
-		set:	function(i){	dragging = container.classList.toggle("dragging", !!i); }
+		get: function(){   return dragging; },
+		set: function(i){  dragging = container.classList.toggle("dragging", !!i); }
 	});
 }(container));
 
@@ -132,32 +132,32 @@ var container	=	document.getElementById("container");
 (container || document.documentElement).addEventListener(Gesture.START, function(event){
 	var startX, startY, x, y, pull,
 
-		thresh		=	125;
+		thresh = 125;
 
 
 	new Drag(event, {
-		start:	function(event, position){
-			x		=	startX	=	position[0],
-			y		=	startY	=	position[1],
-			pull	=	container.pull;
-			container.dragging	=	true;
+		start: function(event, position){
+			x     = startX = position[0],
+			y     = startY = position[1],
+			pull  = container.pull;
+			container.dragging = true;
 		},
 
 
-		move:	function(event, position){
-			x	=	position[0],
-			y	=	position[1];
-			container.pull	=	pull+(x - startX);
+		move: function(event, position){
+			x = position[0],
+			y = position[1];
+			container.pull = pull+(x - startX);
 		},
 
 
-		end:	function(event, position){
+		end: function(event, position){
 			if(position)
-				x	=	position[0],
-				y	=	position[1];
+				x = position[0],
+				y = position[1];
 
 			setTimeout(function(){
-				var delta	=	startX - x;
+				var delta = startX - x;
 				if(delta < -thresh){
 					console.log("Previous slide.");
 					--container.slide;
@@ -170,7 +170,7 @@ var container	=	document.getElementById("container");
 
 				else
 					container.slide = container.slide;
-				container.dragging	=	false;
+				container.dragging = false;
 			}, 40);
 		}
 	});
--- gestures.js
+++ gestures.js
@@ -1,45 +1,45 @@
-var Gesture		=	function(){
+var Gesture = function(){
 
 	/** Open up a fresh list of point arrays. */
-	this.pointLists		=	[[]];
+	this.pointLists     = [[]];
 
 	/** Provide an easy pointer to the Gesture's first point list (the one most frequently used). */
-	this.points			=	this.pointLists[0];
+	this.points         = this.pointLists[0];
 };
 
 
 /** Determine if we're even running on a touch-enabled device. */
-var touch				=	"ontouchstart" in document.documentElement;
-Gesture.TOUCH_ENABLED	=	touch;
+var touch               = "ontouchstart" in document.documentElement;
+Gesture.TOUCH_ENABLED   = touch;
 
 /** Resolve what event handlers we're gonna need. */
-Gesture.START			=	touch ? "touchstart"	: "mousedown";
-Gesture.MOVE			=	touch ? "touchmove"		: "mousemove";
-Gesture.END				=	touch ? "touchend"		: "mouseup";
-Gesture.CANCEL			=	touch ? "touchcancel"	: null;
+Gesture.START           = touch ? "touchstart"  : "mousedown";
+Gesture.MOVE            = touch ? "touchmove"   : "mousemove";
+Gesture.END             = touch ? "touchend"    : "mouseup";
+Gesture.CANCEL          = touch ? "touchcancel" : null;
 
 /** Not sure about these two: are they even standards-compliant/needed? */
-Gesture.ENTER			=	touch ? "touchenter"	: "mouseover";
-Gesture.LEAVE			=	touch ? "touchleave"	: "mouseout";
+Gesture.ENTER           = touch ? "touchenter"  : "mouseover";
+Gesture.LEAVE           = touch ? "touchleave"  : "mouseout";
 
 
 /** Push an event's Cartesian coordinates onto the instance's points array. */
-Gesture.prototype.push	=	function(event){
+Gesture.prototype.push = function(event){
 
 	/**
 	 * Check for a touch list. We won't rely on the TOUCH_ENABLED constant,
 	 * since this method might be called using a simulated TouchEvent.
 	 */
 	if(event.touches){
-		var pos, lists =	this.pointLists;
+		var pos, lists = this.pointLists;
 		for(var l = event.touches.length, i = 0; i < l; ++i){
-			var touch	=	event.touches[i];
+			var touch = event.touches[i];
 
 			/** Store the first touch in the TouchEvent. */
-			if(!pos) pos =	[touch.pageX, touch.pageY];
+			if(!pos) pos = [touch.pageX, touch.pageY];
 
-			if(!lists[i])	lists[i] = [[touch.pageX, touch.pageY]];
-			else			lists[i].push([touch.pageX, touch.pageY]);
+			if(!lists[i]) lists[i]   = [[touch.pageX, touch.pageY]];
+			else          lists[i].push([touch.pageX, touch.pageY]);
 		}
 	}
 
@@ -60,34 +60,32 @@ Gesture.prototype.push	=	function(event){
  * @param {Function} func - The callback to attach.
  * @param {Array} targets - An optional list of DOM elements to add an event listener to. Defaults to document.documentElement
  */
-Gesture.prototype.on	=	function(events, func, targets){
-	var	events	=	Array.isArray(events) ? events : [events],
-		targets	=	targets || [],
-
-		l		=	events.length,
-		i		=	0, target;
-
+Gesture.prototype.on = function(events, func, targets){
+	var events = Array.isArray(events) ? events : [events],
+	targets    = targets || [],
+	l          = events.length,
+	i          = 0, target;
+	
 	for(; i < l; ++i){
-		target	=	targets[i] || document.documentElement;
-		if(target.attachEvent)	target.attachEvent("on"+events[i], func);	/* <= IE8 */
-		else					target.addEventListener(events[i], func);	/* W3 */
+		target = targets[i] || document.documentElement;
+		if(target.attachEvent) target.attachEvent("on"+events[i], func);  /* <= IE8 */
+		else                   target.addEventListener(events[i], func);  /* W3 */
 	}
 
 	return func;
 };
 
 /** Helper method for removing one or more event callbacks. Takes the same arguments as the "on" method. */
-Gesture.prototype.off	=	function(events, func, targets){
-	var	events	=	Array.isArray ? events : [events],
-		targets	=	targets || [],
-		
-		l	=	events.length,
-		i	=	0, target;
-
+Gesture.prototype.off = function(events, func, targets){
+	var events = Array.isArray ? events : [events],
+	targets    = targets || [],
+	l          = events.length,
+	i          = 0, target;
+	
 	for(; i < l; ++i){
-		target	=	targets[i] || document.documentElement;
-		if(target.detachEvent)	target.detachEvent("on"+events[i], func);		/* <= IE8 */
-		else					target.removeEventListener(events[i], func);	/* W3 */
+		target = targets[i] || document.documentElement;
+		if(target.detachEvent)  target.detachEvent("on"+events[i], func);      /* <= IE8 */
+		else                    target.removeEventListener(events[i], func);   /* W3 */
 	}
 
 	return func;
@@ -96,42 +94,40 @@ Gesture.prototype.off	=	function(events, func, targets){
 
 
 
-var Drag	=	function(ev, args){
+var Drag = function(ev, args){
 	if(!ev) throw new ArgumentError("Cannot instantiate a Drag gesture without an event object.");
 
+	var start = args.start,
+	move      = args.move,
+	end       = args.end,
+	cancel    = args.cancel,
 
-	var	start	=	args.start,
-		move	=	args.move,
-		end		=	args.end,
-		cancel	=	args.cancel,
-
-
-	doc		=	document.documentElement,
-	$this	=	this,
+	doc       = document.documentElement,
+	$this     = this,
 
 
-	onMove	=	this.on(Gesture.MOVE, function(ev){
-		var pos	=	$this.push(ev);
+	onMove = this.on(Gesture.MOVE, function(ev){
+		var pos = $this.push(ev);
 		if(move) move.call($this, ev, pos);
 	}),
 
 
-	onEnd	=	this.on([Gesture.END, Gesture.CANCEL, "blur"], function(ev){
-		var pos	=	$this.push(ev);
+	onEnd = this.on([Gesture.END, Gesture.CANCEL, "blur"], function(ev){
+		var pos = $this.push(ev);
 
-		doc.removeEventListener(Gesture.MOVE,	onMove);
+		doc.removeEventListener(Gesture.MOVE, onMove);
 		$this.off([Gesture.END, Gesture.CANCEL, "blur"], onEnd, [,,window])
 
 		if(end) end.call(this, ev, pos);
 	}, [,,window]);
 
 
-	var pos	=	this.push(ev);
+	var pos = this.push(ev);
 
 	/** If given a start handler, trigger it now. */
-	if(start)	start.call(this, ev, pos);
+	if(start) start.call(this, ev, pos);
 };
 
 
-Drag.prototype				=	new Gesture();
-Drag.prototype.constructor	=	Drag;
+Drag.prototype             = new Gesture();
+Drag.prototype.constructor = Drag;
